8254ddce382a8587f2ba02a8c349de2b2dbbd451
Miguel Fiais <miguel.fiais10@gmail.com>
2024-05-30 13:16:06 -0300

    feat: add theme and theme typing

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 interface ButtonContainerProps {
     variant: ButtonVariant
@@ -6,16 +6,13 @@ interface ButtonContainerProps {
 
 export type ButtonVariant = "primary" | "secondary" | "danger" | "success"
 
-const ButtonVariants = {
-    primary: "purple",
-    secondary: "orange",
-    danger: "red",
-    success: "green"
-}
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`   
     width: 100px;
     height: 40px;
-
-    ${props => css`background-color: ${ButtonVariants[props.variant]}`}
+    border-radius: 4px;
+    border: 0;
+    margin: 8px;
+    background-color: ${props => props.theme.primary};
+    color: ${props => props.theme.text};
+    
 `
