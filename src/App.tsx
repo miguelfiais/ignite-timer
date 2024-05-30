15df45a1b4c058e41c70a2dfc45c4cd25865dd34
Miguel Fiais <miguel.fiais10@gmail.com>
2024-05-30 13:18:14 -0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
+      
       <h1>Hello World</h1>
       <Button  variant="primary" />
       <Button  variant="secondary" />
-
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
