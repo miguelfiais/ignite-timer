f0b2959f6a1be3f03bcbe6f0e9af1b5950a036e2
Miguel Fiais <miguel.fiais10@gmail.com>
2024-06-02 13:32:13 -0300

    feat: add zod to validate forms

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 import {
   CountdownContainer,
   FormContainer,
@@ -10,11 +12,28 @@ import {
   TaskInput,
 } from './styles'
 
-export const Home = () => {
-  const { register, handleSubmit, watch } = useForm()
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
 
-  function handleCreateNewCycle(data) {
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+export const Home = () => {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
+    reset()
   }
 
   const task = watch('task')
@@ -41,8 +60,6 @@ export const Home = () => {
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             step={5}
-            min={5}
-            max={60}
             type="number"
             id="minutesAmount"
             placeholder="00"
